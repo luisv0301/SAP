9b31fa119c34dfe51eea6ea8b1b15d646f27c052
Luis Vasquez <luisvbdr0301@gmail.com>
2021-08-26 17:47:31 -0430

    feat: animations added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger);
 const hamburMenu = document.querySelector(".nav__hambur-menu");
 const navMobile = document.querySelector(".nav-mobile");
 
@@ -8,3 +9,55 @@ hamburMenu.addEventListener("click", () => {
 
   navMobile.classList.toggle("visible");
 });
+
+//ANIMATIONS
+//tween
+gsap.from(".image-animation", {
+  scale: 1.2,
+  opacity: 0,
+  stagger: {
+    from: "random",
+    amount: 2,
+  },
+});
+//Scrool-trigger animations
+const sections = gsap.utils.toArray(".section");
+
+sections.forEach((section) => {
+  const title = section.querySelector(".section__tittle");
+  const text = section.querySelector(".section__description");
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: section,
+      start: "center bottom",
+      end: "bottom bottom",
+      scrub: true,
+      markers: true,
+    },
+  });
+  tl.from(title, {
+    opacity: 0,
+    x: 500,
+  }).from(text, {
+    opacity: 0,
+    x: 500,
+  });
+});
+
+const tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".section-customers",
+    start: "top center",
+    end: "+=400",
+    scrub: true,
+    markers: true,
+  },
+});
+tl2
+  .from(".animate-numbers-1", {
+    color: "#475569",
+  })
+  .from(".animate-numbers-2", {
+    color: "#475569",
+  });
